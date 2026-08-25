a3844c0686a2c801c33c05cbb1872b58d1c1fe37
Sambhav <sambhavjain8840@gmail.com>
2026-08-25 14:41:23 +0530

    Changed hardcoded backend api to use env varaibale

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -13,7 +13,7 @@
 import axios from "axios";
 const api = axios.create({
   headers: { "Content-Type": "application/json" },
-  baseURL: "http://localhost:4400",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
